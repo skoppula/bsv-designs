169b5c3b9ff475c93918df4dc9b3d31af22afa28
Skanda Koppula <skanda.koppula@gmail.com>
2017-11-06 11:00:51 -0500

    write ternary weights to mem

--- networkeval/float_to_fxpt.py
+++ networkeval/float_to_fxpt.py
@@ -6,6 +6,7 @@ transcribed_weights = np.where(tmp == -2, np.ones(tmp.shape), tmp)
 with open('weights.binarymem.txt', 'w') as f1:
     with open('weights.ascii.txt', 'w') as f2:
         for i, row in enumerate(transcribed_weights):
-            nums = ['{0:02b}'.format(num) for num in list(row)]
-            f1.write(''.join(nums))
-            f2.write(' '.join(list(rand_weights[i])))
+            nums = ['{0:02b}'.format(int(num)) for num in list(row)]
+            f1.write(''.join(nums) + '\n')
+            nums = [str(num) if len(str(num)) == 2 else ' ' + str(num) for num in list(rand_weights[i])]
+            f2.write(' '.join(nums) + '\n')
