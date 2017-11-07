48aa0c4ff61fd429a7b1273c0a36c38c7c145371
Skanda Koppula <skanda.koppula@gmail.com>
2017-11-06 23:06:27 -0500

    finish fxpt to binary converter

--- networkeval/LayerEval.bsv
+++ networkeval/LayerEval.bsv
@@ -50,8 +50,8 @@ module mkLayerEval( LayerEvalIfc#(n_pes, n_cols, prec_int, prec_dec) );
 
     BRAM_Configure weightCfgRAM = defaultValue;
     BRAM_Configure featCfgRAM = defaultValue;
-    weightCfgRAM.loadFormat = tagged Binary "weights.txt";
-    featCfgRAM.loadFormat = tagged Binary "features.txt";
+    weightCfgRAM.loadFormat = tagged Binary "weights.testing.txt";
+    featCfgRAM.loadFormat = tagged Binary "features.testing.txt";
     BRAM1Port#(Bit#(TLog#(n_cols)), Bit#(TAdd#(prec_int, prec_dec))) featureBRAM <- mkBRAM1Server(featCfgRAM);
     BRAM1Port#(Bit#(TLog#(TAdd#(n_pes,3))), Bit#(TMul#(n_cols,2))) weightBRAM <- mkBRAM1Server(weightCfgRAM);
 
--- networkeval/float_to_fxpt.py
+++ networkeval/float_to_fxpt.py
@@ -1,12 +1,72 @@
 import numpy as np
 
+def bin_pad(s, length):
+    if len(s) >= length: return s
+    return '0'*(length - len(s)) + s
+
+def conv_fxpt(num, prec_int=2, prec_dec=6):
+    # add case where decimal part fixed '1
+    upper_bound = 2**(prec_int - 1) - 2**(-prec_dec)
+    lower_bound = -2**(prec_int - 1)
+    total_prec = prec_int + prec_dec
+    if num < lower_bound or num > upper_bound:
+        print("range clipping", num, lower_bound, upper_bound)
+    num = max(num, lower_bound)
+    num = min(num, upper_bound)
+    scaled_num = int(round(num * 2**(prec_dec)))
+    if scaled_num < 0:
+        pos_scaled_num = -1*scaled_num
+        int_bin_str = bin(pos_scaled_num)[2:][-total_prec:]
+        int_bin_str = bin_pad(int_bin_str, total_prec)
+        inverse_bit_str = ''.join(['0' if c == '1' else '1' for c in int_bin_str])
+        int_repr_twos_complement = int(inverse_bit_str, 2) + 1
+        twos_comp_bin_str = bin(int_repr_twos_complement)[-total_prec:]
+        return bin_pad(twos_comp_bin_str, total_prec)
+    else:
+        int_bin_str = bin(scaled_num)[2:][-total_prec:]
+        return bin_pad(int_bin_str, total_prec)
+
+def run_tests():
+    pos_tests = [
+    (0.500000,'00100000'),
+    (0.25000,'00010000'),
+    (0.68750,'00101100'),
+    (0.00000,'00000000'),
+    (0.00000,'00000000'),
+    (0.68750,'00101100'),
+    (0.09375,'00000110'),
+    ]
+
+    neg_tests = [
+    (-1.00000,'11000000'),
+    (-0.25000,'11110000'),
+    (-0.09375,'11111010'),
+    ]
+
+    for num,truth in neg_tests:
+        print(num, conv_fxpt(num), truth)
+        print()
+
 rand_weights = np.random.randint(3, size=(8, 8)) - 1
 tmp = -2*rand_weights
 transcribed_weights = np.where(tmp == -2, np.ones(tmp.shape), tmp)
+
+pos_consts = np.random.rand(rand_weights.shape[0])*2 - 1
+neg_consts = np.random.rand(rand_weights.shape[0])*2 - 1
+bias = np.random.rand(rand_weights.shape[0])*2 - 1
+
+n_bits_weight_row = rand_weights.shape[1]*2
 with open('weights.binarymem.txt', 'w') as f1:
     with open('weights.ascii.txt', 'w') as f2:
         for i, row in enumerate(transcribed_weights):
             nums = ['{0:02b}'.format(int(num)) for num in list(row)]
             f1.write(''.join(nums) + '\n')
+            f1.write(bin_pad(conv_fxpt(pos_consts[i]), n_bits_weight_row) + '\n')
+            f1.write(bin_pad(conv_fxpt(neg_consts[i]), n_bits_weight_row) + '\n')
+            f1.write(bin_pad(conv_fxpt(bias[i]), n_bits_weight_row) + '\n')
             nums = [str(num) if len(str(num)) == 2 else ' ' + str(num) for num in list(rand_weights[i])]
             f2.write(' '.join(nums) + '\n')
+            f2.write(str(pos_consts[i]) + '\n')
+            f2.write(str(neg_consts[i]) + '\n')
+            f2.write(str(bias[i]) + '\n')
+
